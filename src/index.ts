e9997e1c6d7602efbb72cbddd8a039882390c840
Ali Waseem <github@aliwaseem.com>
2023-04-15 15:04:57 -0600

    updated host

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ async function main() {
   try {
     dotenv.config();
 
-    await api.listen({ port: 3000 });
+    await api.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
     api.log.error(err);
     process.exit(1);
